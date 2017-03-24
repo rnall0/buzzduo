f0e4447a76b5e51fb3ad1e25a991463352e19f81
Robert Nall <rmn0001@gmail.com>
2017-03-24 06:05:23 -0500

    Update map_roster.R
    Fix the quotes from copy from blog

--- R/map_roster.R
+++ R/map_roster.R
@@ -5,7 +5,7 @@ library(mapdata)
 library(ggplot2)
 library(ggsn)
 
-roster<-read.csv(“C:\\path\\to\\data\\Springroster2017.csv”)
+roster<-read.csv("C:\\path\\to\\data\\Springroster2017.csv")
 
 #geocode with google
 counter<-0
@@ -14,14 +14,14 @@ roster.geocode=NULL
 for (i in 1:nrow(roster)){
  require(httr)
  require(rjson)
- url<-”http://maps.googleapis.com/maps/api/geocode/json"
+ url<-"http://maps.googleapis.com/maps/api/geocode/json"
  addr<-paste(roster[i, 8], roster[i,9], sep=”, “)
- response <- GET(url,query=list(sensor=”FALSE”,address=addr))
- json <- fromJSON(content(response,type=”text”))
- cat(paste (‘“‘, addr, ‘“\n’, sep = “”))
+ response <- GET(url,query=list(sensor="FALSE",address=addr))
+ json <- fromJSON(content(response,type="text"))
+ cat(paste ('"', addr, '"\n', sep =""))
  counter <<- counter + 1;
- message(paste(counter, “of”, total))
- loc <- (json[‘results’][[1]][[1]]$geometry$location)
+ message(paste(counter, "of", total))
+ loc <- (json['results'][[1]][[1]]$geometry$location)
  ID<-roster[i,1]
  Number<-roster[i,2]
  Name<-roster[i,3]
@@ -33,7 +33,7 @@ for (i in 1:nrow(roster)){
  State<-roster[i,9]
  Lat<-loc$lat
  Lon<-loc$lng
- df<-data.frame(ID, Number, Name, Position, Height, Weight, Class,       City, State, Lat, Lon)
+ df<-data.frame(ID, Number, Name, Position, Height, Weight, Class,City, State, Lat, Lon)
  roster.geocode<-rbind(roster.geocode, df)
 }
 
@@ -41,22 +41,22 @@ for (i in 1:nrow(roster)){
 states <- map_data("state")
 roster.map<-ggplot(data = states) + 
   geom_polygon(aes(x = long, y = lat, group = group), color = "white") +
-  geom_point(data = roster.geocode, aes(x = roster.geocode$Lon, y = roster.geocode$Lat), color = " dark orange", size = 2) +
+  geom_point(data = roster.geocode, aes(x = roster.geocode$Lon, y = roster.geocode$Lat), color = "dark orange", size = 2) +
   coord_fixed(1.3) +
   ggtitle("2017 Auburn Spring Football Roster Hometowns")
   
 #map point density
 roster.map<-ggplot(data = states) + 
- geom_polygon(aes(x = long, y = lat, group = group), color = “white”) + 
- stat_density2d(data=roster.geocode,aes(roster.geocode$Lon,roster.geocode$Lat, fill= ..level..), alpha =0.5, geom=”polygon”)+
- geom_point(data=roster.geocode, aes(roster.geocode$Lon, roster.geocode$Lat), colour=”dark orange”, alpha = 1, size=1)+
+ geom_polygon(aes(x = long, y = lat, group = group), color = "white") + 
+ stat_density2d(data=roster.geocode,aes(roster.geocode$Lon,roster.geocode$Lat, fill= ..level..), alpha =0.5, geom="polygon")+
+ geom_point(data=roster.geocode, aes(roster.geocode$Lon, roster.geocode$Lat), colour="dark orange", alpha = 1, size=1)+
  coord_fixed(1.3) +
- ggtitle(“2017 Auburn Spring Football Roster Hometowns”)+
+ ggtitle("2017 Auburn Spring Football Roster Hometowns")+
  guides(fill=FALSE)
  
  #scale bar
- scale.bar <- roster.map + scalebar(location=”bottomright”,y.min=27, y.max=32, x.min=-120, x.max=-110, dist=1000, 
-                                    dd2km= TRUE, model=’WGS84', st.dist=.3)
+ scale.bar <- roster.map + scalebar(location="bottomright",y.min=27, y.max=32, x.min=-120, x.max=-110, dist=1000, 
+                                    dd2km= TRUE, model='WGS84', st.dist=.3)
                                     
   #north arrow
   north2(scale.bar, x=.2, y=.3, symbol=8)
